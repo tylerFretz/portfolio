23bfa13c1525f2fc15413f379c201bd8f9c8f856
tyler fretz <fretztyler@gmail.com>
2021-06-26 12:25:49 -0400

    debug heroku

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.static(path.join(__dirname + '/client/build')));
+app.use(express.static(path.join(__dirname + '/client/public')));
 app.use('/images', express.static('images'));
 app.use(express.json());
 app.use(middleware.requestLogger);
@@ -23,7 +23,7 @@ app.use('/api/contact', contactRouter);
 app.use('/api/download', downloadRouter);
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
 app.use(middleware.unknownEndpoint);
