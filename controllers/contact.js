8fbfaaed8d907e32890a047eb709172924530dd4
tyler fretz <fretztyler@gmail.com>
2022-01-24 14:54:49 -0500

    uibuild

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,43 +1,46 @@
 const { body } = require('express-validator');
-const nodemailer = require('nodemailer');
 const config = require('../utils/config');
 const contactRouter = require('express').Router();
+import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 
-// https://stackoverflow.com/questions/19877246/nodemailer-with-gmail-and-nodejs
-
-const transporter = nodemailer.createTransport({
-	service: 'Hotmail',
-	auth: {
-		user: config.SMTP_USER,
-		pass: config.SMTP_PASS
-	}
-});
-
+const sesClient = new SESClient({ region: 'us-east-1' });
 
 contactRouter.post('/',
 	body('firstName').not().isEmpty().trim().escape(),
 	body('lastName').not().isEmpty().trim().escape(),
 	body('email').isEmail().normalizeEmail(),
 	body('message').not().isEmpty().trim().escape(),
-	(req, res) => {
+	async (req, res) => {
 		const { body } = req;
 
-		transporter.sendMail({
-			from: config.SMTP_USER,
-			to: 'fretztyler@gmail.com',
-			subject: 'Portfolio contact',
-			html: `<h3>Name: ${body.firstName} ${body.lastName}</h3>
-			   <h3>Email: ${body.email}</h3>
-			   <p>Message: ${body.message}</p>`
-		}, (error) => {
-			if (error) {
-				console.log(error);
-				res.json({ status: "ERROR" });
-			} else {
-				res.json({ status: "Message Sent" });
-			}
-		});
+		const params = {
+			Destination: {
+				ToAddresses: [ config.SMTP_USER ]
+			},
+			Message: {
+				Body: {
+					Html: {
+						Charset: 'UTF-8',
+						Data: `<h3>Name: ${body.firstName} ${body.lastName}</h3>
+						<h3>Email: ${body.email}</h3>
+						<p>Message: ${body.message}</p>`
+					}
+				},
+				Subject: {
+					Charset: 'UTF-8',
+					Data: 'Portfolio contact'
+				}
+			},
+			Source: config.SMTP_USER
+		};
 
-	});
+		try {
+			const data = await sesClient.send(new SendEmailCommand(params));
+			res.json({ status: 'Message sent' });
+		} catch (err) {
+			console.log(err);
+			res.json({ status: "Error sending message" });
+		}
+});
 
 module.exports = contactRouter;
